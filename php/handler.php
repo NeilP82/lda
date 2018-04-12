402bd97596974aca5cdac2516bc148061c2ee3ce
NeilP82 <npaveling@texasrealtors.com>
2018-04-12 11:42:33 -0500

    php email testing

--- php/handler.php
+++ php/handler.php
@@ -1,10 +1,22 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if($_POST){
     $name = $_POST['form_name'];
     $email = $_POST['form_email'];
     $message = $_POST['form_msg'];
 
 //send email
-    mail("neilpaveling@hotmail.com", "Email from:" .$email, $message);
-}
-?>
+   if(mail(("neilpaveling@hotmail.com", "Email from:".$name, $email, $message);{
+   		echo "success ";
+    }
+   else
+     {
+       echo "Failure";
+     }
+   }
+
+
+ ?>
