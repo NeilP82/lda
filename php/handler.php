7d36d1fb36d6eb56abcc70e36337dd12c9736226
NeilP82 <npaveling@texasrealtors.com>
2018-04-12 11:10:21 -0500

    php email testing

--- php/handler.php
+++ php/handler.php
@@ -1,26 +1,10 @@
 <?php
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
-	/*
-	Tested working with PHP5.4 and above (including PHP 7 )
+if($_POST){
+    $name = $_POST['form_name'];
+    $email = $_POST['form_email'];
+    $message = $_POST['form_msg'];
 
- 	*/
-	require_once './vendor/autoload.php';
-
-use FormGuide\Handlx\FormHandler;
-
-
-$pp = new FormHandler();
-
-$validator = $pp->getValidator();
-$validator->fields(['name','email'])->areRequired()->maxLength(50);
-$validator->field('email')->isEmail();
-$validator->field('comments')->maxLength(6000);
-
-
-
-
-$pp->sendEmailTo('neilpaveling@hotmail.com'); // ← Your email here
-
-echo $pp->process($_POST);
+//send email
+    mail("neilpaveling@hotmail.com", "Email from:" .$email, $message);
+}
+?>
